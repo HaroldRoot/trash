f03dbabea3868b79b0a0c7a9766491514946626b
HaroldRoot <ngoksausi618@gmail.com>
2024-04-27 08:50:06 -0400

    Replace strcmp with strncmp in builtin.c

--- builtin.c
+++ builtin.c
@@ -7,7 +7,7 @@
 
 int is_builtin_command(char *command)
 {
-	if (strcmp(command, "exit") == 0) {
+	if (strncmp(command, "exit", strlen("exit")) == 0) {
 		return 1;
 	}
 	// Add other builtin commands here
@@ -16,7 +16,7 @@ int is_builtin_command(char *command)
 
 void handle_builtin_command(char *command)
 {
-	if (strcmp(command, "exit") == 0) {
+	if (strncmp(command, "exit", strlen("exit")) == 0) {
 		printf("Exiting trash...\n");
 		exit(EXIT_SUCCESS);
 	}
